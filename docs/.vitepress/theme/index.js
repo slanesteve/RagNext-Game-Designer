38c26c9f0199ff8892cc88a503e90aec686d5d86
slanesteve <steve.l.lane@gmail.com>
2026-06-05 09:25:55 -0400

    docs: add medium-zoom support for interactive image previews

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,9 @@
 import DefaultTheme from 'vitepress/theme'
-import { h } from 'vue'
+import { h, onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
 import Comments from './Comments.vue'
+import './custom.css'
 
 export default {
   extends: DefaultTheme,
@@ -8,5 +11,22 @@ export default {
     return h(DefaultTheme.Layout, null, {
       'doc-after': () => h(Comments)
     })
+  },
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      // Initialize mediumZoom on all images inside the content markdown block (.vp-doc)
+      mediumZoom('.vp-doc img', {
+        background: 'rgba(0, 0, 0, 0.85)'
+      })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
   }
 }
+
