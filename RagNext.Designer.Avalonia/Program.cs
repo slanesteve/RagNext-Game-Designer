a40b99ee580320adab2a173cfea965d1d96dc566
slanesteve <steve.l.lane@gmail.com>
2026-06-15 21:26:50 -0400

    Support rendering backend arguments in Program.cs, rename macOS output binary to RagNext

--- RagNext.Designer.Avalonia/Program.cs
+++ RagNext.Designer.Avalonia/Program.cs
@@ -9,16 +9,50 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
+    public static void Main(string[] args) => BuildAvaloniaApp(args)
         .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
+    public static AppBuilder BuildAvaloniaApp() => BuildAvaloniaApp(Array.Empty<string>());
+
+    public static AppBuilder BuildAvaloniaApp(string[] args)
+    {
+        var builder = AppBuilder.Configure<App>()
+            .UsePlatformDetect();
+
+        bool forceSoftware = false;
+        bool forceGl = false;
+
+        foreach (var arg in args)
+        {
+            if (arg.Equals("--software", StringComparison.OrdinalIgnoreCase))
+                forceSoftware = true;
+            else if (arg.Equals("--opengl", StringComparison.OrdinalIgnoreCase))
+                forceGl = true;
+        }
+
+        if (forceSoftware)
+        {
+            builder.With(new AvaloniaNativePlatformOptions
+            {
+                RenderingMode = new[] { AvaloniaNativeRenderingMode.Software }
+            });
+            Console.WriteLine("[INFO] Forcing Avalonia Software rendering mode.");
+        }
+        else if (forceGl)
+        {
+            builder.With(new AvaloniaNativePlatformOptions
+            {
+                RenderingMode = new[] { AvaloniaNativeRenderingMode.OpenGl }
+            });
+            Console.WriteLine("[INFO] Forcing Avalonia OpenGL rendering mode.");
+        }
+
+        return builder
 #if DEBUG
             .WithDeveloperTools()
 #endif
             .WithInterFont()
             .LogToTrace();
+    }
 }
