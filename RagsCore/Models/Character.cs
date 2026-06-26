912031a1a3f9ca7389fbc48d779210048f310204
slanesteve <steve.l.lane@gmail.com>
2026-06-26 12:36:27 -0400

    Feat: Add Gender combobox to the character details database view and map it to Character model

--- RagsCore/Models/Character.cs
+++ RagsCore/Models/Character.cs
@@ -14,6 +14,19 @@ namespace RagsCore.Models
         private int _health = 100;
         public int Health { get => _health; set => SetProperty(ref _health, value); }
 
+        private List<string> _genders = new() { "Male", "Female", "Non-binary", "Other" };
+        public List<string> Genders { get => _genders; set => SetProperty(ref _genders, value); }
+
+        public string Gender
+        {
+            get => Properties.TryGetValue("Gender", out var g) ? g : "Male";
+            set
+            {
+                Properties["Gender"] = value;
+                OnPropertyChanged(nameof(Gender));
+            }
+        }
+
         private Room? _startingRoom = null;
         public Room? StartingRoom
         {
