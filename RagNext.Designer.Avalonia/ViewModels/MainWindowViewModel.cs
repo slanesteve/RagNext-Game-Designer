97bc521cb9a2c87465598f4dd6c26bffac07fa64
slanesteve <steve.l.lane@gmail.com>
2026-07-12 07:41:18 -0400

    Diagnostics: Convert Theme loader logs to System.Diagnostics.Debug.WriteLine for Visual Studio Output Window capture

--- RagNext.Designer.Avalonia/ViewModels/MainWindowViewModel.cs
+++ RagNext.Designer.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -1342,8 +1342,8 @@ namespace RagNext.Designer.Avalonia.ViewModels
 
             App.GameChanged += (g) => 
             { 
-                Console.WriteLine($"[DEBUG-THEME] Loaded BackgroundAssetId: '{g?.Theme?.BackgroundAssetId}', FrameAssetId: '{g?.Theme?.FrameAssetId}'");
-                Console.WriteLine($"[DEBUG-THEME] MediaAssets Count: {g?.MediaAssets?.Count}");
+                System.Diagnostics.Debug.WriteLine($"[DEBUG-THEME] Loaded BackgroundAssetId: '{g?.Theme?.BackgroundAssetId}', FrameAssetId: '{g?.Theme?.FrameAssetId}'");
+                System.Diagnostics.Debug.WriteLine($"[DEBUG-THEME] MediaAssets Count: {g?.MediaAssets?.Count}");
                 _isProjectLoading = true;
                 CurrentGame = g; 
                 OnPropertyChanged(nameof(Player));
